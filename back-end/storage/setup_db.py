e1cbd8f3d8274abec4091d1e235b9edb491fa10c
Amanuel Sisay <amanuelsisay.as@gmail.com>
2024-12-02 10:03:11 +0300

    (feat): add missing packages, and auto folder creation

--- back-end/storage/setup_db.py
+++ back-end/storage/setup_db.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+import os  # Import the os module
 
 Base = declarative_base()
 
 def setup_database():
+    # Create the directory if it doesn't exist
+    os.makedirs('temp_files', exist_ok=True)  # Create the folder
+
     engine = create_engine('sqlite:///temp_files/ssq.db')
     Base.metadata.create_all(engine)  # Create the table if it doesn't exist
     # Add additional setup steps here
