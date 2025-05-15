dbad3d1beb240291c8e7d36860f7b56255c7cd20
Amanuel Abiy <amanuelsisay.as@gmail.com>
2025-05-15 13:40:16 +0300

    Update config.ts

--- front-end/src/utils/config.ts
+++ front-end/src/utils/config.ts
@@ -6,7 +6,7 @@ export interface Config {
 // Configuration object
 const config: Config = {
     // Get the backend URL from environment variables, or use a default if not set
-    backendUrl: process.env.BACKEND_URL || 'https://2a4a-23-94-117-101.ngrok-free.app', // Fallback to localhost if not set
+    backendUrl: process.env.BACKEND_URL || 'https://codebase-rag-backend.amanabiy.tech/', // Fallback to localhost if not set
 
     // Get the port number from environment variables or use 5000 by default
     port: parseInt(process.env.REACT_APP_PORT || '5000', 10), // Default to 5000 if not set
